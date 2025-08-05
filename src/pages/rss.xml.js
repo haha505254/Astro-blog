b36712843ed2e5c35bc7a22d66fb8e18087cb923
haha505254 <haha505254@gmail.com>
2025-08-06 06:06:55 +0800

    刪除：移除 Markdown Style Guide 範例文章

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,13 +4,14 @@ import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function GET(context) {
 	const posts = await getCollection('blog');
+	const publishedPosts = posts.filter(post => !post.data.draft);
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: posts.map((post) => ({
+		items: publishedPosts.map((post) => ({
 			...post.data,
-			link: `/blog/${post.id}/`,
+			link: `/blog/${post.slug}/`,
 		})),
 	});
 }
